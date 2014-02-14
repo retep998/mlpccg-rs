0a9a68853f7e24c8ccef742c859a754a75ec84d5
retep998 <retep998@verizon.net>
2014-02-14 11:39:56 -0500

    Can now easily change font size, and also accounts for console height.
    Signed-off-by: retep998 <retep998@verizon.net>

--- src/ascii/ascii.cpp
+++ src/ascii/ascii.cpp
@@ -109,12 +109,13 @@ namespace nlp {
         double const gamma_factor{12.92};
         double const gamma_decode_threshhold{0.04045};
         double const gamma_encode_threshhold{0.0031308};
-        unsigned conwidth{};
-        unsigned const cwidth{6};
-        unsigned const cheight{8};
+        unsigned maxwidth{};
+        unsigned maxheight{};
+        unsigned cwidth{6};
+        unsigned cheight{8};
         double smoothness{};
         sys::path asset_path{};
-        sys::path const chars_name{"chars" + std::to_string(cwidth) + "x" + std::to_string(cheight) + ".png"};
+        sys::path chars_name{};
         std::array<color, 0x10> const colors{{{0x00, 0x00, 0x00}, {0x80, 0x00, 0x00}, {0x00, 0x80, 0x00}, {0x80, 0x80, 0x00}, {0x00, 0x00, 0x80}, {0x80, 0x00, 0x80}, {0x00, 0x80, 0x80}, {0xc0, 0xc0, 0xc0}, {0x80, 0x80, 0x80}, {0xff, 0x00, 0x00}, {0x00, 0xff, 0x00}, {0xff, 0xff, 0x00}, {0x00, 0x00, 0xff}, {0xff, 0x00, 0xff}, {0x00, 0xff, 0xff}, {0xff, 0xff, 0xff}}};
         std::array<char16_t, 222> const wchars{{0x0020, 0x0021, 0x0022, 0x0023, 0x0024, 0x0025, 0x0026, 0x0027, 0x0028, 0x0029, 0x002a, 0x002b, 0x002c, 0x002d, 0x002e, 0x002f, 0x0030, 0x0031, 0x0032, 0x0033, 0x0034, 0x0035, 0x0036, 0x0037, 0x0038, 0x0039, 0x003a, 0x003b, 0x003c, 0x003d, 0x003e, 0x003f, 0x0040, 0x0041, 0x0042, 0x0043, 0x0044, 0x0045, 0x0046, 0x0047, 0x0048, 0x0049, 0x004a, 0x004b, 0x004c, 0x004d, 0x004e, 0x004f, 0x0050, 0x0051, 0x0052, 0x0053, 0x0054, 0x0055, 0x0056, 0x0057, 0x0058, 0x0059, 0x005a, 0x005b, 0x005c, 0x005d, 0x005e, 0x0060, 0x0061, 0x0062, 0x0063, 0x0064, 0x0065, 0x0066, 0x0067, 0x0068, 0x0069, 0x006a, 0x006b, 0x006c, 0x006d, 0x006e, 0x006f, 0x0070, 0x0071, 0x0072, 0x0073, 0x0074, 0x0075, 0x0076, 0x0077, 0x0078, 0x0079, 0x007a, 0x007b, 0x007c, 0x007d, 0x007e, 0x00c7, 0x00fc, 0x00e9, 0x00e2, 0x00e4, 0x00e0, 0x00e5, 0x00e7, 0x00ea, 0x00eb, 0x00e8, 0x00ef, 0x00ee, 0x00ec, 0x00c4, 0x00c5, 0x00c9, 0x00e6, 0x00c6, 0x00f4, 0x00f6, 0x00f2, 0x00fb, 0x00f9, 0x00ff, 0x00d6, 0x00dc, 0x00a2, 0x00a3, 0x00a5, 0x20a7, 0x0192, 0x00e1, 0x00ed, 0x00f3, 0x00fa, 0x00f1, 0x00d1, 0x00aa, 0x00ba, 0x00bf, 0x2310, 0x00ac, 0x00bd, 0x00bc, 0x00a1, 0x00ab, 0x00bb, 0x2591, 0x2592, 0x2593, 0x2502, 0x2524, 0x2561, 0x2562, 0x2556, 0x2555, 0x2563, 0x2551, 0x2557, 0x255d, 0x255c, 0x255b, 0x2510, 0x2514, 0x2534, 0x252c, 0x251c, 0x2500, 0x253c, 0x255e, 0x255f, 0x255a, 0x2554, 0x2569, 0x2566, 0x2560, 0x2550, 0x256c, 0x2567, 0x2568, 0x2564, 0x2565, 0x2559, 0x2558, 0x2552, 0x2553, 0x256b, 0x256a, 0x2518, 0x250c, 0x2588, 0x2584, 0x258c, 0x2590, 0x2580, 0x03b1, 0x00df, 0x0393, 0x03c0, 0x03a3, 0x03c3, 0x00b5, 0x03c4, 0x03a6, 0x0398, 0x03a9, 0x03b4, 0x221e, 0x03c6, 0x03b5, 0x2229, 0x2261, 0x00b1, 0x2265, 0x2264, 0x2320, 0x2321, 0x00f7, 0x2248, 0x00b0, 0x2219, 0x00b7, 0x221a, 0x207f, 0x00b2, 0x25a0}};
         std::vector<coverage> coverages{};
@@ -123,6 +124,7 @@ namespace nlp {
         std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> codecvt{};
         color_double background{0, 0, 0};
         void calc_coverage() {
+            chars_name = "chars" + std::to_string(cwidth) + "x" + std::to_string(cheight) + ".png";
             auto && in = sf::Image{};
             in.loadFromFile((asset_path / chars_name).string());
             auto index = unsigned{0};
@@ -173,7 +175,7 @@ namespace nlp {
         color_double gamma(sf::Color const & p_color) {
             auto const mult = p_color.a / gamma_mult;
             auto const imult = 1 - mult;
-            return color_double{gamma_decode(p_color.r * mult), gamma_decode(p_color.g * mult), gamma_decode(p_color.b * mult)} + background * imult;
+            return color_double{gamma_decode(p_color.r * mult), gamma_decode(p_color.g * mult), gamma_decode(p_color.b * mult)} +background * imult;
         }
         void calc_combos() {
             auto const && fg_colors = std::array<uint8_t, 16>{{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}};
@@ -226,6 +228,7 @@ namespace nlp {
             auto tty = uv::tty::create(loop);
             tty.write(u8chars);
             tty.write("\n");
+            std::cin.get();
         }
         void display_results() {
             auto loop = uv::loop::create();
@@ -245,9 +248,16 @@ namespace nlp {
             auto && out = std::ofstream((asset_path / sys::path{"motd.txt"}).string(), std::ios::binary);
             auto imgwidth = in.getSize().x;
             auto imgheight = in.getSize().y;
-            auto xratio = static_cast<double>(imgwidth) / conwidth;
+            auto conwidth = maxwidth;
+            auto xratio = static_cast<double>(imgwidth) / maxwidth;
             auto yratio = xratio * cheight / cwidth;
             auto conheight = static_cast<unsigned>(imgheight / yratio);
+            if (conheight > maxheight) {
+                conheight = maxheight;
+                yratio = static_cast<double>(imgheight) / maxheight;
+                xratio = yratio * cwidth / cheight;
+                conwidth = static_cast<unsigned>(imgwidth / xratio);
+            }
             auto shrunk = std::vector<color_double>{};
             auto const total_pixels = conwidth * conheight;
             shrunk.resize(total_pixels);
@@ -266,7 +276,7 @@ namespace nlp {
                     shrunk[y * conwidth + x] = sum * mult;
                 }
             }
-            auto adjust = [&total_pixels, &shrunk](unsigned const & x, unsigned const & y, color_double const & err, double const & mult) {
+            auto adjust = [&](unsigned const & x, unsigned const & y, color_double const & err, double const & mult) {
                 auto const index = y * conwidth + x;
                 if (index < total_pixels) {
                     shrunk[index] += err * mult;
@@ -340,8 +350,11 @@ namespace nlp {
             mapping["smoothness"] = [](std::string const & str) {
                 smoothness = std::stod(str);
             };
-            mapping["width"] = [](std::string const & str) {
-                conwidth = static_cast<unsigned>(std::stoul(str));
+            mapping["maxwidth"] = [](std::string const & str) {
+                maxwidth = static_cast<unsigned>(std::stoul(str));
+            };
+            mapping["maxheight"] = [](std::string const & str) {
+                maxheight = static_cast<unsigned>(std::stoul(str));
             };
             mapping["bgr"] = [](std::string const & str) {
                 background.r = std::stod(str);
@@ -351,6 +364,12 @@ namespace nlp {
             };
             mapping["bgb"] = [](std::string const & str) {
                 background.b = std::stod(str);
+            };
+            mapping["cwidth"] = [](std::string const & str) {
+                cwidth = static_cast<unsigned>(std::stoul(str));
+            };
+            mapping["cheight"] = [](std::string const & str) {
+                cheight = static_cast<unsigned>(std::stoul(str));
             };
             auto && regex = std::regex{"(.*)=(.*)", std::regex_constants::ECMAScript};
             while (std::getline(file, line)) {
