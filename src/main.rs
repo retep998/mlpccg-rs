633e6a624a3e450a9c5496f1cedeb161b6a5cfa4
Peter Atashian <retep998@gmail.com>
2014-06-15 04:39:29 -0400

    Accepts new connections
    Signed-off-by: Peter Atashian <retep998@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
+#![feature(phase)]
+#[phase(plugin)] extern crate green;
 
+green_start!(main)
 
 fn main() {
-    println!("Hello world");
+    use std::io::{Acceptor, Listener, TcpListener, TcpStream};
+    use std::sync::{Arc, RWLock};
+    print!("{}", include_str!("motd.txt"));
+    let listener = TcpListener::bind("0.0.0.0", 273);
+    let mut acceptor = listener.listen();
+    fn handle_client(mut stream: TcpStream) {
+        match stream.peer_name() {
+            Ok(name) => println!("Connection from {}", name),
+            Err(e) => fail!("Failed to get name: {}", e),
+        }
+    }
+    println!("Starting server");
+    for stream in acceptor.incoming() {
+        match stream {
+            Ok(stream) => spawn(proc() handle_client(stream)),
+            Err(e) => fail!("Failed to accept connection: {}", e),
+        }
+    }
 }
