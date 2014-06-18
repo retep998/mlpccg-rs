7aa38ad6cccee29d6615850bd0c52cbf07b33a35
Peter Atashian <retep998@gmail.com>
2014-06-18 09:52:21 -0400

    Committing progress so far.
    Signed-off-by: Peter Atashian <retep998@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,65 @@
-#![feature(phase)]
-#[phase(plugin)] extern crate green;
 
-use std::collections::{SmallIntMap};
-use std::io::{Acceptor, BufReader, IoError, Listener, TcpListener, TcpStream};
+#![feature(phase, macro_rules)]
+
+#[phase(plugin)]
+extern crate green;
+
+use std::collections::{HashMap, SmallIntMap};
+use std::comm::{Receiver, Sender};
+use std::io::{Acceptor, BufferedReader, BufferedWriter, BufReader, IoResult, Listener, MemWriter, TcpListener, TcpStream, stdin};
 use std::sync::{Arc, RWLock};
 
-green_start!(main)
-
 type Opcode = u16;
+type Id = u32;
+
+macro_rules! try(
+    ($e:expr, $f:expr) => (match $e { Ok(e) => e, Err(_) => return $f })
+)
+
+green_start!(main)
+fn main() {
+}
+
+
+fn packet_sender(recv: Receiver<Vec<u8>>, tcp: TcpStream) {
+    let mut buf = BufferedWriter::new(tcp);
+    for msg in recv.iter() {
+        println!("Got packet to send!");
+        try!(buf.write_be_u32(msg.len() as u32), ());
+        try!(buf.write(msg.as_slice()), ());
+        try!(buf.flush(), ());
+    }
+}
+enum Message {
+    RemovePlayer(Id),
+    SendPacket(Id, Vec<u8>),
+    AddPlayer(TcpStream),
+    Shutdown,
+}
+fn packet_receiver(send: Sender<Message>, tcp: TcpStream, id: Id) {
+    let mut buf = BufferedReader::new(tcp);
+    (|| {
+        let len = try!(buf.read_be_u32(), ());
+        let data = try!(buf.read_exact(len as uint), ());
+        try!(send.send_opt(SendPacket(id, data)), ());
+    })();
+    try!(send.send_opt(RemovePlayer(0)), ());
+}
+fn broker(recv: Receiver<Message>) {
+    
+}
+fn listener() {
+
+}
+fn commands(send: Sender<Message>) {
+    let cin = stdin();
+    
+}
+
+/*
+type Opcode = u16;
+type Id = u32;
+type Handler = fn(&RWLock<Server>, &RWLock<Player>, BufReader);
 
 fn main() {
     print!("{}", include_str!("motd.txt"));
@@ -27,20 +79,49 @@ fn main() {
     }
 }
 
+fn new_packet(opcode: Opcode) -> IoResult<MemWriter> {
+    let mut buf = MemWriter::new();
+    try!(buf.write_be_u16(opcode));
+    Ok(buf)
+}
+
+#[deriving(Share)]
+struct Player {
+    name: String,
+    id: Id,
+}
+
+impl Player {
+    fn handle_ping(server: &RWLock<Server>, player: &RWLock<Player>, buf: BufReader) {
+        
+    }
+    fn send(player: &RWLock<Player>, buf: &MemWriter) -> IoResult<()> {
+        let data = buf.get_ref();
+        let mut lock = player.write();
+        let stream = &mut lock.deref_mut().stream;
+        try!(stream.write_be_u32(data.len() as u32));
+        try!(stream.write(data));
+        Ok(())
+    }
+}
+
 struct Server {
-    handlers: SmallIntMap<fn()>,
+    handlers: SmallIntMap<Handler>,
+    players: HashMap<Id, Arc<RWLock<Player>>>,
 }
 
 impl Server {
     fn new() -> Server {
-        unimplemented!();
+        Server{handlers: Server::gen_handlers(), players: HashMap::new()}
     }
-    fn gen_handlers() -> SmallIntMap<fn()> {
+    fn gen_handlers() -> SmallIntMap<Handler> {
+        let mut map = SmallIntMap::new();
         unimplemented!();
+        map
     }
 }
 
-fn handle_client(mut stream: TcpStream) -> Result<(), IoError> {
+fn handle_client(mut stream: TcpStream) -> IoResult<()> {
     println!("Connection from {}", try!(stream.peer_name()));
     loop {
         let length = try!(stream.read_be_u32());
@@ -59,3 +140,4 @@ fn handle_client(mut stream: TcpStream) -> Result<(), IoError> {
         }
     }
 }
+*/
